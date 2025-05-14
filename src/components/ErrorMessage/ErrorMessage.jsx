467f6c937ebb7c363a1a9c8e6e0e2e7076077b75
DmytroKaniuchok <kanyuchekdmitry@gmail.com>
2025-05-14 15:10:40 +0300

    add changes

--- src/components/ErrorMessage/ErrorMessage.jsx
+++
@@ -1,7 +0,0 @@
-import styles from "./ErrorMessage.module.css";
-
-export default function ErrorMessage({
-  message = "Something went wrong! Try refreshing the page!",
-}) {
-  return <div className={styles.error}>{message}</div>;
-}
